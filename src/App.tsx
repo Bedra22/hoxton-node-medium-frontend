92bcaa96168dd377f53d7866cfb8fda8967b13c2
Bedra Kraja <bedra.kraja22@gmail.com>
2022-09-15 09:58:44 +0200

    tu

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
   const [allPosts, setAllPosts] = useState([])
+  const [commentsOnPosts, setCommentsOnPosts] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:5000/posts")
@@ -11,9 +12,24 @@ function App() {
       .then(postsFromServere => setAllPosts(postsFromServere))
   }, [])
 
+  useEffect(() => {
+    fetch("http://localhost:5000/comments")
+      .then(resp => resp.json())
+      .then(commentsFromServers => setCommentsOnPosts(commentsFromServers))
+  }, [])
+
   return (
     <div className="App">
-      <h1>Medium BLOG</h1>
+      <h1 className='h1-title'>Medium BLOG</h1>
+      <div className='first-part'>
+        <div className='left-part'>
+          <h1>Stay curious.</h1>
+          <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+        </div>
+        <div className='right-part'>
+          <img src="https://images.pexels.com/photos/266583/pexels-photo-266583.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" />
+        </div>
+      </div>
       <div className='all-posts'>
         <ul>
           {allPosts.map(item => (
@@ -27,7 +43,6 @@ function App() {
                 <p>{item.writenContent}</p>
               </div>
               <div className='like-comment'>
-                {/* <div>{item.comment}</div> */}
                 <h4>{item.likesInTotal}</h4>
               </div>
             </li>
